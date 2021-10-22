ce95a7c52cf6ed512e6a03125df688f705b05581
plosslaw <plosslaw@gmail.com>
2021-10-22 17:43:12 +0800

    22 oct 2021 update

--- start_dev.bat
+++ start_dev.bat
@@ -3,6 +3,7 @@ mode con: cols=80 lines=20
 
 cd ALweb/build/dev
 start cmd.exe /k "npm run dev"
+start cmd.exe /k "sass --watch stylingSrc:public/static/editor/css"
 
 cd ../../learn
 start cmd.exe /k "npm run dev"
