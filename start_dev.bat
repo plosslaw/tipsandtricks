a3c5490eb4844b4c0a00064b28055def2312e54f
plosslaw <plosslaw@gmail.com>
2024-04-03 09:48:31 +0800

    fix typo in start_dev.bat

--- start_dev.bat
+++ start_dev.bat
@@ -2,15 +2,15 @@
 mode con: cols=80 lines=15
 
 cd ALweb/build/dev
-start cmd.exe /k ""npm install && npm run dev"
+start cmd.exe /k "npm install && npm run dev"
 ::start cmd.exe /k "sass --watch stylingSrc:public/static/editor/css"
 ::start cmd.exe /k "node workers/FolderReportMailWorker"
 
 cd ../../learn
-start cmd.exe /k ""npm install && npm run dev"
+start cmd.exe /k "npm install && npm run dev"
 
-::cd ../mail-server
-::start cmd.exe /k "npm install && npm run dev"
+cd ../mail-server
+start cmd.exe /k "npm install && npm run dev"
 
 cd ..
 start git-bash.exe
